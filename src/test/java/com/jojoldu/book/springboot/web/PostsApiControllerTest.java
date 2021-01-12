9eec2e92a2b4706c12ab8806c046f974dfb95ed1
kceun <eun2ee@naver.com>
2021-01-12 21:52:18 +0900

    JPA 조회/등록/수정 API 추가, Aduiting 기능 추가

--- src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springboot.web;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,13 +11,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -47,7 +52,7 @@ public class PostsApiControllerTest {
         String url = "http://localhost:"+port+"/api/v1/posts";
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url,requestDto,Long.class);
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url,requestDto,Long.class); //post 메서드로 보낸다는 의미
 
         //then
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -59,4 +64,52 @@ public class PostsApiControllerTest {
 
     }
 
+    @Test
+    public void Posts_수정() throws Exception {
+
+        //given
+        Posts savePosts = postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        Long updateId = savePosts.getId();
+        String expectTitle = "title2";
+        String expectContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder().title(expectTitle).content(expectContent).build();
+
+        String url = "http://localhost:"+port+"/api/v1/posts/"+updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectContent);
+
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder().title("title!").content("contents!").build());
+        //when
+        List<Posts> all = postsRepository.findAll();
+
+        //then
+        Posts posts = all.get(0);
+
+        System.out.println(">>>>>>>>>>> createDate="+posts.getCreateDate()+", modified="+posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
+
+    }
 }
